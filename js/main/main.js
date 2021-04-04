73d18f53e28c0b279c5016c97aebdbab630755a4
sawasick <50393229+sawasick@users.noreply.github.com>
2021-04-04 21:55:54 +0900

    update:ヘッダー完成

--- js/main/main.js
+++ js/main/main.js
@@ -48,31 +48,6 @@ function Loadjson() {
 		}
 	);
 }
-// -----スキルアイコンアニメーション-----
-$(window).scroll(function () {
-	const windowHeight = $(window).height();
-	let thisPos;
-
-	$('.js-skill').each(function () {
-		thisPos = $(this).offset().top;
-		const scroll = $(window).scrollTop();
-		const $paId = $(this).parent().attr('id');
-		const per = (100 - jsonSkill[$paId]) * 0.8;
-		const currentHeight = $(this).css('height');
-		if (
-			currentHeight === '120px' &&
-			scroll > thisPos - windowHeight + windowHeight / 3
-		) {
-			$(this).animate(
-				{
-					height: `${per}%`,
-				},
-				{ duration: 1300 }
-			);
-		} else {
-		}
-	});
-});
 
 document.addEventListener(
 	'DOMContentLoaded',
@@ -82,3 +57,58 @@ document.addEventListener(
 	},
 	false
 );
+$(function () {
+	let headerFlag = false;
+	$(window).scroll(function () {
+		const windowHeight = $(window).height();
+		let thisPos;
+
+		// -----ヘッダー表示切り替え-----
+		const $header = $('.header-wrapper');
+		thisPos = $(this).scrollTop();
+		if (thisPos > windowHeight / 2) {
+			if (!headerFlag) {
+				headerFlag = true;
+				$header.toggleClass('is-hide');
+			}
+		} else {
+			if (headerFlag) {
+				headerFlag = false;
+				$header.toggleClass('is-hide');
+			}
+		}
+
+		// -----スキルアイコンアニメーション-----
+		$('.js-skill').each(function () {
+			thisPos = $(this).offset().top;
+			const scroll = $(window).scrollTop();
+			const $paId = $(this).parent().attr('id');
+			const per = (100 - jsonSkill[$paId]) * 0.8;
+			const currentHeight = $(this).css('height');
+			if (
+				currentHeight === '120px' &&
+				scroll > thisPos - windowHeight + windowHeight / 3
+			) {
+				$(this).animate(
+					{
+						height: `${per}%`,
+					},
+					{ duration: 1300 }
+				);
+			} else {
+			}
+		});
+	});
+});
+
+// -----スムーススクロール-----
+$(function () {
+	$('a[href^="#"]').click(function () {
+		const speed = 500;
+		const href = $(this).attr('href');
+		const target = $(href == '#' || href == '' ? 'html' : href);
+		const position = target.offset().top - $(window).height() / 10;
+		$('html, body').animate({ scrollTop: position }, speed, 'swing');
+		return false;
+	});
+});
