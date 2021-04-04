d07747bd922e0d80b7b5447e7ad13e51b294ad4f
sawasick <50393229+sawasick@users.noreply.github.com>
2021-04-04 16:50:50 +0900

    add: skillsのアニメーション実装

--- js/main/main.js
+++ js/main/main.js
@@ -1,4 +1,6 @@
+let jsonSkill; //jsonファイル読み込み
 function ReplaceFont() {
+	// iOSの時にフォントを変える
 	const $targets = $('.font-extra, .section-title');
 	for (let i = 0, len = $targets.length; i < len; i++) {
 		$targets.eq(i).addClass('js-font-extra-ios');
@@ -7,16 +9,6 @@ function ReplaceFont() {
 function CheckiOS() {
 	// OSを判定→iOS,iPadOSならfont-familyを切り替える
 	const ua = navigator.userAgent;
-	// if (
-	// 	/iPad|iPhone|iPod/.test(ua) ||
-	// 	(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
-	// ) {
-	// }
-	// const $targets = $('.font-extra, .section-title');
-	// for (let i = 0, len = $targets.length; i < len; i++) {
-	// 	$targets.eq(i).addClass('js-font-extra-ios');
-	// }
-
 	if (
 		ua.indexOf('iPhone') > 0 ||
 		ua.indexOf('iPod') > 0
@@ -40,11 +32,53 @@ function CheckiOS() {
 		ReplaceFont();
 	}
 }
+function Loadjson() {
+	$.ajax({
+		// json読み込み開始
+		type: 'GET',
+		url: 'json/skills.json',
+		dataType: 'json',
+	}).then(
+		function (json) {
+			// jsonの読み込みに成功した時
+			jsonSkill = json;
+		},
+		function () {
+			//jsonの読み込みに失敗した時
+		}
+	);
+}
+// -----スキルアイコンアニメーション-----
+$(window).scroll(function () {
+	const windowHeight = $(window).height();
+	let thisPos;
+
+	$('.js-skill').each(function () {
+		thisPos = $(this).offset().top;
+		const scroll = $(window).scrollTop();
+		const $paId = $(this).parent().attr('id');
+		const per = 100 - jsonSkill[$paId];
+		const currentHeight = $(this).css('height');
+		if (
+			currentHeight === '120px' &&
+			scroll > thisPos - windowHeight + windowHeight / 2
+		) {
+			$(this).animate(
+				{
+					height: `${per}%`,
+				},
+				{ duration: 1300 }
+			);
+		} else {
+		}
+	});
+});
 
 document.addEventListener(
 	'DOMContentLoaded',
 	function () {
 		CheckiOS();
+		Loadjson();
 	},
 	false
 );
